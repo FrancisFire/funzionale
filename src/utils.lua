ea6b2d60c096bcc6a535a118f4721570b49b6e7b
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2020-05-01 16:38:55 +0200

    Added directions table

---
+++ src/utils.lua
@@ -0,0 +1,6 @@
+directions = {
+    function(x, y) return x + 1, y + 1 end,
+    function(x, y) return x - 1, y + 1 end,
+    function(x, y) return x + 1, y - 1 end,
+    function(x, y) return x - 1, y - 1 end
+}
