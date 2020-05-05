d284d943c4fd251edc26de8ab0f4ed7cd5a3c0fa
Sbyner <tmmscrltt@gmail.com>
2020-05-05 18:27:12 +0200

    [FIX] - Syntax errors fixed according to IDE

--- src/game.lua
+++ src/game.lua
@@ -1,35 +1,40 @@
 utils = require "utils"
 
 function move(maze, row, column, steps, life)
-    local newSteps = steps + 1 
+    local newSteps = steps + 1
     local result = applyCellEffect(localMaze, row, column, life)
-    if(result.lose) then return {maze = maze, steps = newSteps, life = newLife, win = false} 
-    elseif(result.win) then return {maze = maze, steps = newSteps, life = newLife, win = true} 
-    else 
-        local tracedMaze = tracedMaze(maze, row, column)
-        
+    if(result.lose) then return {maze = maze, steps = newSteps, life = newLife, win = false}
+    elseif(result.win) then return {maze = maze, steps = newSteps, life = newLife, win = true}
+    else
+        local tracedMaze = traceMaze(maze, row, column)
+
         results = {}
         for k,newDir in pairs(utils.Directions)do
             local x,y = newDir(row, column)
-            
+
             local subRes = move(tracedMaze, x, y, steps, newLife)
             if(subRes.win) then table.insert(results, subRes) end
         end
         local result = calcOptimalResult(results)
         if(result == nil) then return {maze = maze, steps = newSteps, life = newLife, win = false}
         else return result
+    end
+end
 end
 
 function calcOptimalResult(resTable)
-    
-    local func = function (tmpTable, minimum)
+    local func
+    func = function (tmpTable, minimum)
         if(#tmpTable == 0) then return minimum end
         local head = table.remove(tmpTable, 1)
         if(minimum == nil) then return func(tmpTable, head)
         else if((head.steps < minimum.steps) or (head.steps == minimum.steps and head.life < minimum.life)) then return func(tmpTable, head)
     end
+
     return func(resTable, nil) end
 end
+end
+
 
 
 function traceMaze(maze, row, column)
@@ -72,7 +77,7 @@ function applyCellEffect(maze, row, column, life)
             return life * 2
         end,
         ["f"] = function(life)
-            return life // 2
+            return life / 2
         end,
         ["u"] = function(life)
             return life
